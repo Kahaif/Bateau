746e4a93284a4dc89f0a190ee317bb5f254e8596
KevinF <kevin.ferati@infoteam.ch>
2025-03-16 16:55:59 +0100

    front : translations infrastructure

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,4 +1,7 @@
+/// <reference types="@angular/localize" />
+
 import "@angular/compiler";
+import '@angular/localize/init';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
